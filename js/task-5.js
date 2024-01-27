10c0d7c279c9490ff79e85f62d125d282ce32282
mercury-paws <145927427+mercury-paws@users.noreply.github.com>
2024-01-27 21:34:28 +0100

    js hm 7 4

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorButton = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.body;
+// const body = document.getElementsByTagName("body")[0];
+
+function clickHandler() {
+  color.textContent = getRandomHexColor();
+  body.style.backgroundColor = getRandomHexColor();
+}
+
+changeColorButton.addEventListener("click", clickHandler);
